c4b3bcaa27c0931d4670c85bd758cb16f20002f2
vkavinmech <vrkavinkumar@gmail.com>
2024-10-08 11:48:21 +0530

    kavin updated files

--- MOM_BenchTraining/kavin/screenshot.cy.js
+++
@@ -1,8 +0,0 @@
-describe('Capture screenshot', () => {
-    it('Should perform screenshot', () => {
-        cy.visit('https://www.google.com/');
-        cy.screenshot('page');
-        cy.wait(3000);
-        cy.get('.lnXdpd').screenshot('element');
-    });
-}); 
