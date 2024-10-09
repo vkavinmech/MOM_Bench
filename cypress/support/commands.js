0dd109e258a15a00e4abc87753c80cfcdeea29bb
vkavinmech <vrkavinkumar@gmail.com>
2024-10-09 13:41:51 +0530

    plug in added

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -26,36 +26,5 @@
 
 /// <reference types="cypress" />
 
-Cypress.Commands.add('getIframe',(iframe)=>{
-    return cy.get(iframe)
-    .its('0.contentDocument.body')
-    .should('be.visible')
-    .then(cy.wrap);
-})
+// <reference types="cypress-xpath" />
 
-// custom command for clicking link using lable(lable means text present in html )
-
-Cypress.Commands.add('clickLink',(label)=> {
-    cy.get("a").contains(label).click();
-})
-
-//overwrite contains()
-
-/*Cypress.Commands.overwrite('contains',(orginalFn, subject, filter, text, options = {})=> {
-   //describe if a filter argument was passed
-    if(typeof text === 'object'){
-        options = text
-        text = filter
-        filter = undefined
-    }
-    options.matchCase = false      // use this it will not a case sensitive
-    
-    return orginalFn(subject, filter, text, options)  
-})*/
-//custom command for login
-
-Cypress.Commands.add('loginapp',(email,password)=> {
-    cy.get(":nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input").type(email);
-    cy.get(":nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input").type(password);
-    cy.get(".oxd-button").click();
-})
