f1052a3dfb566f3f47ad74f5a4cd0529d05c8715
vkavinmech <vrkavinkumar@gmail.com>
2024-10-10 11:33:56 +0530

    files updated

--- cypress/e2e/vidya/csslocators.cy.js
+++ cypress/e2e/vidya/csslocators.cy.js
@@ -3,10 +3,10 @@ describe('CSSLocators', ()=>{
 it("CSSLocator", () =>{
 
 
-  cy.visit("www.meesho.com")
+  cy.visit("https://www.meesho.com/")
 
-  cy.get(".sc-iqPaeV > .sc-eDvSVe").type("T-shirts for kids")
-  cy.get(".search-results > .sc-ftTHYK > :nth-child(1) > .sc-eDvSVe").click()
+  cy.get("input[placeholder='Try Saree, Kurti or Search by Product Code']").type("T-shirts for kids")
+  cy.get("body > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > p:nth-child(2)").click()
   
 })
 
