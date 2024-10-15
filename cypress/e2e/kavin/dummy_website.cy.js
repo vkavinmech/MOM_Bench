f242365712bf0e69c5753d252aa3212cfdbafde5
vkavinmech <vrkavinkumar@gmail.com>
2024-10-15 17:41:41 +0530

    files added by kavin

--- cypress/e2e/kavin/dummy_website.cy.js
+++ cypress/e2e/kavin/dummy_website.cy.js
@@ -36,103 +36,7 @@ describe('Dummy Ticket positive Scenarios', () => {
         cy.get("body > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > section:nth-child(3) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > form:nth-child(1) > div:nth-child(6) > span:nth-child(1) > a:nth-child(1)").click();
         cy.title().should('eq','Contact - Dummy ticket')
     })
-
-    it.only('should perform place order', () => {
-        cy.get("#travname").type(data.firstname);
-        cy.get("#travlastname").type(data.lastname);
-        cy.get("#dob").click();
-        cy.get("select[aria-label='Select month']").select('Jul');
-        cy.get("select[aria-label='Select year']").select('1996');
-        cy.get("tbody tr:nth-child(3) td:nth-child(3) a:nth-child(1)").click();
-        cy.get("#sex_1").check();
-        cy.get("#fromcity").type(data.fromcity);
-        cy.get("#tocity").type(data.tocity);
-        cy.get('#departon').click();
-        cy.get("select[aria-label='Select month']").select('Oct');
-        cy.get("select[aria-label='Select year']").select('2024');
-        cy.get("tbody tr:nth-child(5) td:nth-child(5) a:nth-child(1)").click();
-        cy.get("#deliverymethod_3").check();
-        cy.get("#billname").type(data.billingName);
-        cy.get("#billing_phone").type(data.phone);
-        cy.get("#billing_email").type(data.emailValid);
-        cy.get("#select2-billing_country-container").click();
-        cy.get("input[role='combobox']").type('India').type('{enter}')
-        cy.get("#billing_address_1").type(data.houseNo);
-        cy.get("#billing_address_2").type(data.village);
-        cy.get("#billing_city").type(data.city);
-        cy.xpath("(//span[@id='select2-billing_state-container'])[1]").click({ force: true });
-        cy.get("input[role='combobox']").type('Tamil Nadu').type('{enter}')
-        cy.get("#billing_postcode").type(data.pincode);
-        cy.get('#place_order').click();
-        cy.url().should('contain', "/paymentoptions");
-    });
-  
-    it('should perform payment using netbanking', () => {
-        cy.get("#travname").type(data.firstname);
-        cy.get("#travlastname").type(data.lastname);
-        cy.get("#dob").click()
-        cy.get("select[aria-label='Select month']").select('Jul');
-        cy.get("select[aria-label='Select year']").select('1996');
-        cy.get("tbody tr:nth-child(3) td:nth-child(3) a:nth-child(1)").click();
-        cy.get("#sex_1").check();
-        cy.get("#fromcity").type(data.fromcity);
-        cy.get("#tocity").type(data.tocity);
-        cy.get('#departon').click();
-        cy.get("select[aria-label='Select month']").select('Oct');
-        cy.get("select[aria-label='Select year']").select('2024');
-        cy.get("tbody tr:nth-child(5) td:nth-child(5) a:nth-child(1)").click();
-        cy.get("#deliverymethod_3").check();
-        cy.get("#billname").type(data.billingName);
-        cy.get("#billing_phone").type(data.phone);
-        cy.get("#billing_email").type(data.emailValid);
-        cy.get("#select2-billing_country-container").click();
-        cy.get("input[role='combobox']").type('India').type('{enter}')
-        cy.get("#billing_address_1").type(data.houseNo);
-        cy.get("#billing_address_2").type(data.village);
-        cy.get("#billing_city").type(data.city);
-        cy.xpath("(//span[@id='select2-billing_state-container'])[1]").click({ force: true });
-        cy.get("input[role='combobox']").type('Tamil Nadu').type('{enter}')
-        cy.get("#billing_postcode").type(data.pincode);
-        cy.get('#place_order').click();
-        cy.get("#amount-impression").should('contain', '990');
-        cy.get('[data-testid="all-options-cta"]').click();
-        cy.get("li:nth-child(4) span:nth-child(2)").click();
-        cy.url().should('include', '/netbanking');
-    });
-  
-    it('should perform payment using wallet', () => {
-        cy.get("#travname").type(data.firstname);
-        cy.get("#travlastname").type(data.lastname);
-        cy.get("#dob").click();
-        cy.get("select[aria-label='Select month']").select('Jul');
-        cy.get("select[aria-label='Select year']").select('1996');
-        cy.get("tbody tr:nth-child(3) td:nth-child(3) a:nth-child(1)").click();
-        cy.get("#sex_1").check();
-        cy.get("#fromcity").type(data.fromcity);
-        cy.get("#tocity").type(data.tocity);
-        cy.get('#departon').click();
-        cy.get("select[aria-label='Select month']").select('Oct');
-        cy.get("select[aria-label='Select year']").select('2024');
-        cy.get("tbody tr:nth-child(5) td:nth-child(5) a:nth-child(1)").click();
-        cy.get("#deliverymethod_3").check();
-        cy.get("#billname").type(data.billingName);
-        cy.get("#billing_phone").type(data.phone);
-        cy.get("#billing_email").type(data.emailValid);
-        cy.xpath("(//span[@id='select2-billing_state-container'])[1]").click({ force: true });
-        cy.get("input[role='combobox']").type('India').type('{enter}')
-        cy.get("#billing_address_1").type(data.houseNo);
-        cy.get("#billing_address_2").type(data.village);
-        cy.get("#billing_city").type(data.city);
-        cy.get('#select2-billing_state-container').click();
-        cy.get("input[role='combobox']").type('Tamil Nadu').type('{enter}')
-        cy.get("#billing_postcode").type(data.pincode);
-        cy.get('#place_order').click();
-        cy.get("#amount-impression").should('contain', '990');
-        cy.get('[data-testid="all-options-cta"]').click();
-        cy.get(':nth-child(3) > .card-num').click();
-        cy.url().should('include','/cashcard');
-    });
-
+    
     it('should validate the affiliate program', () => {
         cy.get("body > div:nth-child(1) > div:nth-child(2) > section:nth-child(3) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > a:nth-child(5) > span:nth-child(1)").click();
         cy.wait(2000)
@@ -144,4 +48,3 @@ describe('Dummy Ticket positive Scenarios', () => {
         cy.title().should('eq','Standard program | Affiliate signup | Dummyticket');
       });
   });
-  
