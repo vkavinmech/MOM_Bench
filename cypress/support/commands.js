aad14d29c88ad84a57cc8da439fdb58303123109
vidyashreekaup <vidyaabbedi@gmail.com>
2024-10-17 10:24:30 +0530

    file added by vidya

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -64,3 +64,10 @@ Cypress.Commands.add('loginapp',(email,password)=> {
     cy.get(".oxd-button").click();
 })
 
+
+Cypress.Commands.add('login', (username, password) => { 
+    cy.visit("http://the-internet.herokuapp.com/login")
+    cy.get("input[name='username']").type(username)
+    cy.get("input[name='password']").type(password)
+    cy.get("button[type='submit']").click()
+ })
