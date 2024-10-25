3157399bf7c3f2c76739a604f69105a478f96a05
vidyashreekaup <vidyaabbedi@gmail.com>
2024-10-25 10:52:01 +0530

    file added by divya

---
+++ cypress/e2e/Divya_Ram/myfirstscript.cy.js
@@ -0,0 +1,12 @@
+
+describe('My First Test', () => {
+    it('Verify title - positive test', () => {
+      cy.visit("https://opensource-demo.orangehrmlive.com/")
+      cy.title().should('eq', 'OrangeHRM')
+    })
+
+    it('Verify title - negative test', () => {
+        cy.visit("https://opensource-demo.orangehrmlive.com/")
+        cy.title().should('not.eq', 'OrangeHRM12')
+      })
+  })
