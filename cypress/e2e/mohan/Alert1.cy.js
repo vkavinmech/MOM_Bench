f654b2a45e4c88130e6492c42040efb19cedcb9a
vkavinmech <vrkavinkumar@gmail.com>
2024-12-21 17:24:47 +0530

    files added

--- cypress/e2e/mohan/Alert1.cy.js
+++ cypress/e2e/mohan/Alert1.cy.js
@@ -1,6 +1,6 @@
 describe("Alert",()=>
 {
-    it("Normal Alerts",()=>
+    it("Normal ",()=>
     {
         cy.visit("https://the-internet.herokuapp.com/javascript_alerts");
         cy.get("button[onclick='jsAlert()']").click();
