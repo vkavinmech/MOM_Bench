e441fa8de57ce241d8522a0a59d2428b9663372c
kavinkumar <150880481+vkavinmech@users.noreply.github.com>
2025-01-29 12:45:47 +0530

    Update Dropdown.cy.js

--- cypress/e2e/Jayasurya/Dropdown.cy.js
+++ cypress/e2e/Jayasurya/Dropdown.cy.js
@@ -14,17 +14,4 @@ it('Should Verify the Dropdown dynamic', () => {
     cy.get('#searchInput').type('Delhi')
     cy.get('.suggestion-title').contains('Delhi University').click({force: true});
 });
-it.only('Should Verify the Dropdown dynamic', () => {
-    cy.visit("https://www.google.com/");
-    cy.xpath("//textarea[@id='APjFqb']").type('cypress automation')
-    cy.wait(3000)
-    cy.get('div.wM6W7d>span').should('have.length',13)
-
-    cy.get('div.wM6W7d>span').each(($el,index,$list) =>{
-        if($el.text()=='cypress automation'){
-            cy.wrap($el).click()
-        }
-        cy.get('#APjFqb').should('have.value','cypress automation')   // verify all cypress automation in dropdown
-    })
-});
 });
